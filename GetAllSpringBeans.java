90406a4ab1296049f378cda5c409a15485d89b58
Mohit Sharma <mohitsharma603@gmail.com>
2023-09-01 18:32:19 +0530

    Update GetAllSpringBeans.java

--- GetAllSpringBeans.java
+++ GetAllSpringBeans.java
@@ -1,1 +1,20 @@
+package com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication{
+  
+	private static ApplicationContext applicationContext;
+
+	public static void main(String[] args) {
+		applicationContext =  SpringApplication.run(SreMonitoringToolApplication.class, args);
+		String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for(String beanName : allBeanNames) {
+            System.out.println("Bean name ->"+beanName);
+        }
+	}
+}
 
