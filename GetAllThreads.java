725b1ac3a00a0d4df0c9414f13fd56fd800b7a44
Mohit Sharma <mohitsharma603@gmail.com>
2025-01-05 11:12:16 +0530

    Update GetAllThreads.java

--- GetAllThreads.java
+++ GetAllThreads.java
@@ -18,5 +18,7 @@ public class GetAllThreads {
 			System.out.printf("%-15s \t %-15s \t %-15d \t %s\n", t.getName(), t.getState(), t.getPriority(), t.isDaemon());
 		}
 	}
-
+	// Signal Dispatcher: this thread handles signals sent by the operating system to the JVM.
+	// Finalizer: this thread performs finalizations for objects that no longer need to release system resources.
+	// Reference Handler: this thread puts objects that are no longer needed into the queue to be processed by the Finalizer thread.
 }
